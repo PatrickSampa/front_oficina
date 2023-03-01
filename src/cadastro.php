7f5259b5596e21f78de6c730c5e25e1e107eb555
Patrick Sampaio <sampa.mendonca@gmail.com>
2023-02-28 22:23:39 -0300

    fase 1

---
+++ src/cadastro.php
@@ -0,0 +1,11 @@
+<h2  style="text-align:center"></h2><br>
+
+<form>
+<div class="d-grid gap-2 col-6 mx-auto">
+  <button class="btn btn-primary rounded-pill" type="button"><a href="cadastroOficina.php" style="color: white; text-decoration:none" >CADASTRAR OFICINA</a></a></button>
+  <button class="btn btn-danger rounded-pill" type="button"><a href="cadastrarCliente.php" style="color: white; text-decoration:none" >CADASTRAR CLIENTE</a></button>
+  <button class="btn btn-success rounded-pill" type="button"><a href="cadastroFuncionario.php" style="color: white; text-decoration:none" >CADASTRAR FUNCIONÁRIO</a></button>
+  <button class="btn btn-warning rounded-pill" type="button"><a href="cadastroVeiculo.php" style="color: white; text-decoration:none" >CADASTRAR VEÍCULO</a></button>
+  
+</div>
+</form>
